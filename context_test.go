076150c6ce6a47045ee05bc2c8a186abf46a6a44
Azzin Maharil <jozzieazin@gmail.com>
2022-04-30 11:01:18 +0700

    first Golang Context

---
+++ context_test.go
@@ -0,0 +1,115 @@
+package golangcontext
+
+import (
+	"context"
+	"fmt"
+	"runtime"
+	"testing"
+	"time"
+)
+
+func TestContext(t *testing.T) {
+	background := context.Background()
+	fmt.Println(background)
+
+	todo := context.TODO()
+	fmt.Println(todo)
+}
+
+func TestContextWithValue(t *testing.T) {
+	contextA := context.Background()
+
+	contextB := context.WithValue(contextA, "b", "B")
+	contextC := context.WithValue(contextA, "c", "C")
+
+	contextD := context.WithValue(contextB, "d", "D")
+	contextE := context.WithValue(contextB, "e", "E")
+
+	contextF := context.WithValue(contextC, "f", "F")
+
+	fmt.Println(contextA)
+	fmt.Println(contextB)
+	fmt.Println(contextC)
+	fmt.Println(contextD)
+	fmt.Println(contextE)
+	fmt.Println(contextF)
+
+	fmt.Println(contextF.Value("f")) // dapat
+	fmt.Println(contextF.Value("c")) // dapat milik parent
+	fmt.Println(contextF.Value("b")) // tidak dapat beda parent
+	fmt.Println(contextA.Value("b")) // tidak bisa mengambil data child
+}
+
+func CreateCounter(ctx context.Context) chan int {
+	destination := make(chan int)
+
+	go func() {
+		defer close(destination)
+		counter := 1
+
+		for {
+			select {
+			case <-ctx.Done():
+				return
+			default:
+				destination <- counter
+				counter++
+				time.Sleep(1 * time.Second)
+			}
+		}
+	}()
+	return destination
+}
+
+func TestContextWithCancel(t *testing.T) {
+	fmt.Println("Total Goroutine :", runtime.NumGoroutine())
+	parent := context.Background()
+	ctx, cancel := context.WithCancel(parent)
+
+	destination := CreateCounter(ctx)
+
+	for n := range destination {
+		fmt.Println("Counter", n)
+		if n == 10 {
+			break
+		}
+	}
+	cancel() // mengirim signal cancle ke context
+	time.Sleep(2 * time.Second)
+
+	fmt.Println("Total Goroutine :", runtime.NumGoroutine())
+}
+
+func TestContextWithTimeout(t *testing.T) {
+	fmt.Println("Total Goroutine :", runtime.NumGoroutine())
+	parent := context.Background()
+	ctx, cancel := context.WithTimeout(parent, 5*time.Second)
+	defer cancel()
+
+	destination := CreateCounter(ctx)
+
+	for n := range destination {
+		fmt.Println("Counter", n)
+	}
+	// mengirim signal cancle ke context
+	time.Sleep(2 * time.Second)
+
+	fmt.Println("Total Goroutine :", runtime.NumGoroutine())
+}
+
+func TestContextWithDeadLine(t *testing.T) {
+	fmt.Println("Total Goroutine :", runtime.NumGoroutine())
+	parent := context.Background()
+	ctx, cancel := context.WithDeadline(parent, time.Now() .Add(5*time.Second))
+	defer cancel()
+
+	destination := CreateCounter(ctx)
+
+	for n := range destination {
+		fmt.Println("Counter", n)
+	}
+	// mengirim signal cancle ke context
+	time.Sleep(2 * time.Second)
+
+	fmt.Println("Total Goroutine :", runtime.NumGoroutine())
+}
